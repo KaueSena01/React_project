c8786858a95c3c20b609f185818dcdab4a27ce5d
KaueSena01 <kauealvessena17@gmail.com>
2021-11-28 08:44:22 -0300

    messages

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,22 @@
+import Message from "../layout/Message"
+import { useLocation } from 'react-router-dom'
+
+// useLocation para acessar o history
+
 function Project() {
-    console.log()
-    return <h1>Project</h1>
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+
+    return(
+        <div>
+            <h1>Meus projetos</h1>
+            {message && <Message msg={message} type="success"/>}
+        </div>
+    )
 }
 
 export default Project
